9c99e273350ed503bd173baebb5d77eac6766e19
unknown <calbodragon>
2019-07-17 12:41:31 -0500

    upload

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/*const about =function(){
+    return<h1>hola mundo geek!</h1>
+}
+const aboutdos=() =>{
+    return <h1>hola mundo geek!</h1>
+}*/
+
+const About =()=> (<h1>hola mundo geek!</h1>)
+
+export default About;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import About from './components/About';
+// que voy a renderixar (el componente)
+ReactDOM.render(<About/>,document.getElementById('app'));
+//donde lo voy  arenderizar
